225f346b1aa1e9bf01b42eddacf9eded75398584
Zeus Ser <93567722+zeus-ser@users.noreply.github.com>
2022-07-12 11:19:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,6 @@ var router = express.Router();
 
 router.get('/', async(req, res) => {
 	res.sendFile(__path + '/views/index.html')
-router.get('/deploy', async(req, res) => {
-	res.sendFile(__path + '/views/deploy-md.html')
-})
 router.get('/css/style.css', async(req, res) => {
 	res.sendFile(__path + '/views/css/style.css')
 })
